fae6dd3710a2cb05ab4fa9922bcc65ee30a7c06b
Shivendra kumar <42783165+shiv7532@users.noreply.github.com>
2025-07-18 01:40:47 +0530

    Add .next/static/chunks/src_app_property_[id]_page_tsx_34437c69._.js 1752783053339

---
+++ .next/static/chunks/src_app_property_[id]_page_tsx_34437c69._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_2b813ca2._.js",
+  "static/chunks/src_8765ee5c._.js"
+],
+    source: "dynamic"
+});
